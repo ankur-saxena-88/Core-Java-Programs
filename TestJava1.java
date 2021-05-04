480e2c4a9f4f74e014ef52aa47560fcfde0fc611
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-05-04 11:01:20 +0530

    Hello World Program 2
    Output:
    
    Hello, Java World!
    
    I'm coming back to the race.

---
+++ TestJava1.java
@@ -0,0 +1,21 @@
+class Ttx2
+{
+    public void m1()
+    {
+        String str = "I'm coming back to the race.";
+
+        System.out.println(str);
+    }
+}
+public class TestJava1
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, Java World!\n");
+
+        Ttx2 obj = new Ttx2();
+
+        // call the m1() method
+        obj.m1();
+    }
+}
